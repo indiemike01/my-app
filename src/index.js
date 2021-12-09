a7fcbb54eaa743afdbcbfa54724e967942ef2559
mihaitrof <mihaitrof@gmail.com>
2021-12-09 15:22:07 +0200

    Add CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+//CSS
+
+import "./index.css";
+
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
     </section>
   );
@@ -11,22 +15,15 @@ function BookList() {
 
 const Book = () => {
   return (
-    <article>
-      <Image />
-      <Author />
-      <Title />
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL906_SR906,600_.jpg"
+        alt=""
+      />
+      <h1>Robert Kennedy</h1>
+      <h4>The real anthony Fauci</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL906_SR906,600_.jpg"
-    alt=""
-  />
-);
-
-const Author = () => <h1>Robert Kennedy</h1>;
-const Title = () => <h1>The real anthony Fauci</h1>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
