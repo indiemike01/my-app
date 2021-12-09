7a0b0b922c05144089fad0be66f17e294fa6e36e
mihaitrof <mihaitrof@gmail.com>
2021-12-09 12:41:42 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => <h4>Billy Doe</h4>;
+const Message = () => <p>This is my sdfaf</p>;
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
