b2329576be4e3f7e25f9f7b33077899b7c618eec
mihaitrof <mihaitrof@gmail.com>
2021-12-09 16:52:07 +0200

    Iterating lists

--- src/index.js
+++ src/index.js
@@ -7,51 +7,37 @@ import "./index.css";
 
 //Set up vars
 
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL906_SR906,600_.jpg",
-  title: "The real anthony Fauci",
-  author: "Robert Kennedy",
-};
-
-const secondBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL906_SR906,600_.jpg",
-  title: "Lilly from the vally",
-  author: "Carmen Gucci",
-};
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL906_SR906,600_.jpg",
+    title: "The real anthony Fauci",
+    author: "Robert Kennedy",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL906_SR906,600_.jpg",
+    title: "Lilly from the vally",
+    author: "Carmen Gucci",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, cum.
-          Autem suscipit recusandae tenetur perspiciatis. Vero quisquam
-          accusamus corrupti quae.
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book img={book.img} title={book.title} author={book.author} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{author}</h1>
       <h4>{title}</h4>
-      {children}
       <p></p>
     </article>
   );
