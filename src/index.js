15fd9ad96745b3ba451d2dcba737ccfed1357c8a
mihaitrof <mihaitrof@gmail.com>
2021-12-10 12:53:38 +0200

    Events

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL906_SR906,600_.jpg",
     title: "The real anthony Fauci",
     author: "Robert Kennedy",
   },
   {
+    id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL906_SR906,600_.jpg",
     title: "Lilly from the vally",
@@ -25,20 +27,34 @@ const books = [
 function BookList() {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book img={book.img} title={book.title} author={book.author} />;
+      {books.map((book, index) => {
+        return <Book key={index} {...book} />;
       })}
     </section>
   );
 }
 
 const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  //onClick, onMouseHover
+  const clickHandler = () => {
+    alert("hello world");
+  };
+
   return (
     <article className="book">
-      <img src={img} alt="" />
+      <img
+        src={img}
+        alt=""
+        onMouseOver={() => {
+          console.log("something");
+        }}
+      />
       <h1>{author}</h1>
       <h4>{title}</h4>
-      <p></p>
+      <button type="button" onClick={clickHandler}>
+        reference
+      </button>
     </article>
   );
 };
